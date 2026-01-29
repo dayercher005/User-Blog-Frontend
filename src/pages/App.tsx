45ab264791c3c29602600808fe8dd757d0eea656
Dayer <dayerxr.cher@gmail.com>
2026-01-29 12:41:34 +0800

    feat: added pages for user frontend

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import { router } from '../routes/routes.tsx';
+import '../styles/index.css';
 
-  return (
-    <>
-    </>
-  )
-}
-
-export default App
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
