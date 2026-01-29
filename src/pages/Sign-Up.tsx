6f1381ac05d02b8c3dc0bb4b35e2bb03062f59ae
Dayer <dayerxr.cher@gmail.com>
2026-01-29 16:57:58 +0800

    feat: minor ui additions

--- src/pages/Sign-Up.tsx
+++ src/pages/Sign-Up.tsx
@@ -1,16 +1,17 @@
-import { GalleryVerticalEnd } from "lucide-react"
-import { SignupForm } from "@/components/Signup-form"
+import { SignupForm } from "@/components/Signup-form.tsx";
+import { ArrowLeftIcon } from "lucide-react";
+import { Button } from "@/components/ui/button.tsx";
+import { Link } from 'react-router';
 export default function SignupPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
-          </a>
+          <Link to="/" className="flex items-center gap-2 font-medium">
+            <Button variant="outline" className="bg-primary text-primary-foreground flex size-8 items-center justify-center rounded-md">
+              <ArrowLeftIcon className="size-4" />
+            </Button>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
