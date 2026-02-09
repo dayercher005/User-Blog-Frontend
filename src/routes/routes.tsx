e040adeb0d70e332f2a61b6ca5be80b68feb9e4d
Dayer <dayerxr.cher@gmail.com>
2026-02-09 13:34:59 +0800

    feat: added error elements for every route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,29 +3,34 @@ import Dashboard from '../pages/Dashboard.tsx';
 import { BlogReader } from '../pages/BlogReader.tsx';
 import LoginPage from '../pages/Log-In.tsx';
 import SignupPage from '../pages/Sign-Up.tsx';
-import { Homepage } from '../pages/Homepage.tsx';
+import { Homepage } from '@/pages/Homepage.tsx';
 import { NotFound } from '@/pages/NotFound.tsx';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Homepage />
+    element: <Homepage />,
+    errorElement: <NotFound />
   },
   {
     path: "/dashboard",
     element: <Dashboard />,
+    errorElement: <NotFound />
   },
   {
     path: "/:blogID",
-    element: <BlogReader />
+    element: <BlogReader />,
+    errorElement: <NotFound />
   },
   {
     path: "/log-in",
     element: <LoginPage />,
+    errorElement: <NotFound />
   },
   {
     path: "/sign-up",
-    element: <SignupPage />
+    element: <SignupPage />,
+    errorElement: <NotFound />
   },
   {
     path: "*",
